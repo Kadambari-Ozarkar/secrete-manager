7e292b04e752c729de0cda85795dcdc3add81b15
Kadambari-Ozarkar <kadambari0809@gmail.com>
2026-08-09 15:50:08 +0530

    Integrate AWS Secrets Manager for database credentials

--- project3-secret-management/application/app.py
+++ project3-secret-management/application/app.py
@@ -1,1 +1,20 @@
+import boto3
+import json
+import mysql.connector
 
+secret_name = "project3/database-credentials"
+region = "ap-south-1"
+
+client = boto3.client("secretsmanager", region_name=region)
+
+response = client.get_secret_value(SecretId=secret_name)
+
+secret = json.loads(response["SecretString"])
+
+db = mysql.connector.connect(
+    host=secret["host"],
+    user=secret["username"],
+    password=secret["password"]
+)
+
+print("Database connected successfully!")
